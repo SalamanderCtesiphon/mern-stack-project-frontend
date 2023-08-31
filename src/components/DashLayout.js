8cf06905748c5d11ddd044aa2d80c979fcfdea48
salamanderctesiphon <swbrookshire@gmail.com>
2023-08-31 14:06:27 -0500

    imported font awesome

--- src/components/DashLayout.js
+++ src/components/DashLayout.js
@@ -1,14 +1,16 @@
-import React from 'react'
 import { Outlet } from 'react-router-dom'
+import DashHeader from './DashHeader'
+import DashFooter from './DashFooter'
 
 const DashLayout = () => {
-  return (
-    <>
-      <div className='dash-container'>
-        <Outlet />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <DashHeader />
+            <div className="dash-container">
+                <Outlet />
+            </div>
+            <DashFooter />
+        </>
+    )
 }
-
 export default DashLayout
